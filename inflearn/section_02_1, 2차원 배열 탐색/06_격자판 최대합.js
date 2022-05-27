cc399859906d9cb03259d1e23f4b8637d9d1b8e2
Wonjun <dittoavec@gmail.com>
2022-05-27 23:56:04 +0900

    22.05.27

--- inflearn/section_02_1, 2차원 배열 탐색/06_격자판 최대합.js
+++ inflearn/section_02_1, 2차원 배열 탐색/06_격자판 최대합.js
@@ -1,27 +1,34 @@
 function solution(arr) {
-    let answer = Number.MIN_SAFE_INTEGER;
-    let n = arr.length;
-    let sum1 = sum2 = 0;
-    for (let i = 0; i < n; i++) {
-        sum1 = sum2 = 0;
-        for (let j = 0; j < n; j++) {
-            sum1 += arr[i][j];
-            sum2 += arr[j][i];
-        }
-        answer = Math.max(answer, sum1, sum2);
-    }
-    sum1 = sum2 = 0;
-    for (let i = 0; i < n; i++) {
-        sum1 += arr[i][i];
-        sum2 += arr[i][n - i - 1];
-    }
+  let answer = Number.MIN_SAFE_INTEGER;
+  let sum1 = 0;
+  let sum2 = 0;
+
+  arr.forEach((num, i) => {
+    sum1 = 0;
+    sum2 = 0;
+    arr.forEach((num, j) => {
+      sum1 += arr[i][j]; // 행
+      sum2 += arr[j][i]; // 열
+    });
     answer = Math.max(answer, sum1, sum2);
-    return answer;
+  });
+
+  sum1 = 0;
+  sum2 = 0;
+  arr.forEach((num, i) => {
+    sum1 += arr[i][i];
+    sum2 += arr[i][arr.length - i - 1];
+  });
+  answer = Math.max(answer, sum1, sum2);
+
+  return answer;
 }
 
-let arr = [[10, 13, 10, 12, 15],
-[12, 39, 30, 23, 11],
-[11, 25, 50, 53, 15],
-[19, 27, 29, 37, 27],
-[19, 13, 30, 13, 19]];
+let arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
 console.log(solution(arr));
