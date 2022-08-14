95ea283684b6f911903a4adb64425efdb5799d90
Wonjun <dittoavec@gmail.com>
2022-08-14 16:33:51 +0900

    직사각형 별찍기

--- programmers/Level_1/직사각형 별찍기.js
+++ programmers/Level_1/직사각형 별찍기.js
@@ -1,13 +1,20 @@
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
 
-    for(let i = 0; i < b; i++) {
-        let temp = ""
-        for(let j = 0; j < a; j++) {
-            temp += '*'
-        }
-        console.log(temp);
+  for (let i = 0; i < b; i++) {
+    let temp = "";
+    for (let j = 0; j < a; j++) {
+      temp += "*";
     }
+    console.log(temp);
+  }
+
+  // 220814
+  const row = "*".repeat(a);
+  for (let i = 0; i < b; i++) {
+    console.log(row);
+  }
 });
