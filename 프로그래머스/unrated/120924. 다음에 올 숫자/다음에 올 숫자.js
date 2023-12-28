4f96fd589913a9d19b3d431b0b7f74beff574f29
Wonjun <dittoavec@gmail.com>
2023-12-29 03:24:01 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,11 +1,7 @@
 function solution(common) {
     const last = common.length -1;
-    const gap = common[last] - common[last - 1];
+    const isAP = common[1] - common[0] === common[2] - common[1]
     
-    if(common[last - 1] - gap === common[last - 2]) {
-        return common[last] + gap
-    } else {
-        return common[last] * (common[last] / common[last - 1])
-    }
- 
+    if(isAP) return common[last] + (common[1] - common[0]);
+    else return common[last] * (common[1] / common[0]); 
 }
