801cc77734baf87c7a7a9b1f6005cf66983e1cde
Wonjun <dittoavec@gmail.com>
2023-12-30 21:02:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,30 +1,5 @@
 function solution(s) {
-    const numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    const strArr = {
-        zero: '0',
-        one: '1',
-        two: '2',
-        three: '3',
-        four: '4',
-        five: '5',
-        six: '6',
-        seven: '7',
-        eight: '8',
-        nine: '9',
-    }
-    
-    let res = '';
-    let tempString = '';
-    
-    for(let i = 0; i < s.length; i++) {
-        if(numArr.includes(s[i])) res += s[i];
-        else tempString += s[i];
-        
-        if(strArr[tempString]) {
-          res+= strArr[tempString];
-          tempString = '';
-        } 
-    }
-
-    return +res;
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    numbers.forEach((num, idx) => s = s.replaceAll(num, idx))
+    return +s;
 }
