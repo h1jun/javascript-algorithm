2f47f61765f5abfd60878ff112ab3b0477894a28
Wonjun <dittoavec@gmail.com>
2023-12-31 01:31:44 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    let answer = 0;
-    
-    for(let i = 1; i < 3500; i++) {
-        if(i * i === n) answer = 1;
-    }
-    return answer ? answer : 2
+    return Math.sqrt(n) % 1 === 0 ? 1 : 2;
 }
