f691f3c0fd607c43a1e2a1008e7e85efd44a6f56
Wonjun <dittoavec@gmail.com>
2023-12-30 21:06:43 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, n) {
+    let answer = '';
+    
+    my_string.split('').forEach((str) => {
+     for(let i = 0; i < n; i++)  answer += str;
+    })
+    
+    return answer;
+}
