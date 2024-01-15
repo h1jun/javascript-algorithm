19e52b2863826d1a763c4f4ef82d2ea879c93b2c
Wonjun <dittoavec@gmail.com>
2024-01-15 23:16:35 +0900

    [level Lv.1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12901. 2016년/2016년.js
+++ 프로그래머스/Lv.1/12901. 2016년/2016년.js
@@ -1,16 +1,13 @@
 function solution(a, b) {
-    let answer = '';
-    let count = -1
-    let date = ['FRI','SAT', 'SUN','MON','TUE','WED','THU'];
+    let date = ['SUN','MON','TUE','WED','THU', 'FRI','SAT'];
     let month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+    let day = b + 4;
+    
     for(let i=0; i<a-1; i++) {
-        count += month[i];
+        day += month[i];
     }
-    count += b;
 
-    let result = count % 7;
-    answer = date[result]
+    let result = day % 7;
 
-    return answer;
+    return date[result];
 }
