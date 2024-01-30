04528f2ba2144b519a6f4c740436e54669550e7c
Wonjun <dittoavec@gmail.com>
2024-01-30 23:12:32 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,6 +1,11 @@
 function solution(numbers, direction) {
+    // const len = numbers.length - 1;
+    // if(direction === 'right') return [numbers[len], ...numbers.slice(0, len)];
+    // else return [...numbers.slice(1, len+1), numbers[0]];
+    
     const len = numbers.length - 1;
-    if(direction === 'right') return [numbers[len], ...numbers.slice(0, len)];
-    else return [...numbers.slice(1, len+1), numbers[0]];
+    if(direction === 'right')  numbers.unshift(numbers.pop());
+    else numbers.push(numbers.shift());
+    return numbers;
     
 }
