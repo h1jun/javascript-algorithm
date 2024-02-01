3ccc46b42daece2e0f420b34d10c9d396c0fa98d
Wonjun <dittoavec@gmail.com>
2024-02-01 23:33:05 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 7.06 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -1,9 +1,6 @@
 function solution(myString) {
-    const arr = myString.split('');
-    const str = myString.split('x');
-    const resArr = [];
-    for(const el of str) {
-        resArr.push(el)
-    }
-    return resArr.sort().filter(el => el !== '')
+    return myString
+                .split('x')
+                .sort()
+                .filter(el => el !== '')
 }
