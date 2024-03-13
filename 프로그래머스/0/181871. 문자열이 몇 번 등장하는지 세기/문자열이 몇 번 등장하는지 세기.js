8e54cf8cf865cef9367a6b518d99f8d8c63f3c85
Wonjun <dittoavec@gmail.com>
2024-03-14 01:03:13 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,10 +1,4 @@
 function solution(myString, pat) {
-    const len = myString.length;
-    let res = 0;
-    
-    for(let i = 0; i < len; i++) {
-        if (i + pat.length > len) continue;
-        if (myString.slice(i, i+pat.length) === pat) res++;
-    }
-    return res;
+    var answer = 0;
+    return answer;
 }
