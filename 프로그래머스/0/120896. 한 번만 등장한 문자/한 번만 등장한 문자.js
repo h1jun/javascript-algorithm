8e168a9ab175e35003bec710c9ad00b0ef2186db
Wonjun <dittoavec@gmail.com>
2024-04-02 00:04:11 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,12 +1,7 @@
 function solution(s) {
-    let str = '';
-    let res = [...s];
-    
-    [...s].forEach((el, idx) => {
-        res[idx] = ''
-        if(res.join('').indexOf(el) ===  -1) str += el;
-        res = [...s]
-    })
-    
-    return [...str].sort().join('')
+    let res = [];
+    for(let str of s) {
+        if(s.indexOf(str) === s.lastIndexOf(str)) res.push(str)
+    }
+    return res.sort().join('');
 }
