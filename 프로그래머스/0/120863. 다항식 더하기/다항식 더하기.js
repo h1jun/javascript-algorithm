72920e5e0f1c746a7de565b066f5c5dc55d899e9
Wonjun <dittoavec@gmail.com>
2024-04-28 23:23:42 +0900

    [level 0] Title: 다항식 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,30 @@
+function solution(polynomial) {
+    let terms = polynomial.split('+').map(term => term.trim());
+    let xCoefficient = 0;
+    let constant = 0;
+
+    terms.forEach(term => {
+        if (term.includes('x')) {
+            let xTerm = term.split('x')[0].trim();
+            if (xTerm === "") xCoefficient += 1;
+            else xCoefficient += parseInt(xTerm);
+        } else {
+            constant += parseInt(term);
+        }
+    });
+
+    let result = '';
+    if (xCoefficient > 0) {
+        if (xCoefficient === 1) result += 'x';
+        else result += `${xCoefficient}x`;
+    }
+    if (constant !== 0) {
+        if (xCoefficient !== 0) {
+            if (constant > 0) result += ` + ${constant}`;
+            else result += ` - ${-constant}`;
+        } else {
+            result += constant;
+        }
+    }
+    return result;
+}
