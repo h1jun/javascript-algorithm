f0b46cd58e5eca61129d39aac876a6bd0c7d1f5f
Wonjun <dittoavec@gmail.com>
2024-04-30 01:56:03 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.13 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,15 +1,9 @@
 function solution(arr, queries) {
-
     queries.forEach(el => {
-        const tempArr = [];
-        for(let i = el[0]; i <= el[1]; i++) {
-            tempArr.push(i)
+        const [s, e, k] = el;
+        for(let i = s; i <= e; i++) {
+            if(i % k === 0) arr[i] += 1;
         }
-        
-        tempArr.forEach(el2 => {
-            if(el2 % el[2] === 0) arr[el2] += 1;
-        })
     })
-    
     return arr;
 }
