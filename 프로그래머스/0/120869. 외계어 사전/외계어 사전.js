f575cadc0410e2482e3c85fb822b31542a66a421
Wonjun <dittoavec@gmail.com>
2024-04-30 00:58:44 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,12 @@
 function solution(spell, dic) {
-    const word = spell.sort().join('');
-    let res = 2;
-    dic.forEach(el => {
-        const dicWord = [...el].sort().join('');
-        if(word === dicWord) res = 1;
-    })
+//     const word = spell.sort().join('');
+//     let res = 2;
+//     dic.forEach(el => {
+//         const dicWord = [...el].sort().join('');
+//         if(word === dicWord) res = 1;
+//     })
  
-    return res;
+//     return res;
+    
+    return dic.filter(v => spell.every(c => v.includes(c))).length ? 1 : 2;
 }
